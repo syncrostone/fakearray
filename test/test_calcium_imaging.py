70be99d436f3de65d4ad4415843d108227653dc2
syncrostone <jasminetstone@gmail.com>
2016-08-07 22:27:33 -0400

    update test and version number because pypi was uploaded from wrong file in version 1.0.1

--- test/test_calcium_imaging.py
+++ test/test_calcium_imaging.py
@@ -1,8 +1,12 @@
 import pytest
 from fakearray import calcium_imaging
 
-def test_calcium_imaging():
+def test_calcium_imaging_data():
 	data, series, truth = calcium_imaging(shape=(100,300), n=5, t=50, seed=42, noise=0.5, withparams=True)
 	assert data.shape[0]==50
 	assert data.shape[1]==100
 	assert data.shape[2]==300
+
+def test_calcium_imaging_truth():
+	data, series, truth = calcium_imaging(shape=(100,300), n=5, t=50, seed=42, noise=0.5, withparams=True)
+	assert [[coord[0] in range(0,data.shape[1]) and coord[1] in range(0,data.shape[2]) for coord in neuron]for neuron in truth]
